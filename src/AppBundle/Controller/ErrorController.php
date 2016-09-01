d16f8042d1f2b4936ae72bd199e9dbf6f195943e
Kamil Bednarek <kamillo1888@gmail.com>
2016-09-01 21:38:50 +0200

    Charts

--- src/AppBundle/Controller/ErrorController.php
+++ src/AppBundle/Controller/ErrorController.php
@@ -23,6 +23,126 @@ class ErrorController extends Controller
     }
 
     /**
+     * @Route("/stats", name="error_stats")
+     */
+    public function statsAction()
+    {
+        $collection = $this->getDataProvider();
+
+        // 24 hours stats
+        $createdWithin24hours = $collection->aggregate([
+            [
+                '$group' => [
+                    '_id' => [
+                        'month' => [
+                            '$month' => '$occurred'
+                        ],
+                        'day' => [
+                            '$dayOfMonth' => '$occurred'
+                        ],
+                        'hour' => [
+                            '$hour' => '$occurred'
+                        ]
+                    ],
+                    'count' => [
+                        '$sum' => 1
+                    ]
+                ]
+            ]
+        ], [
+            '$sort' => [
+                'occurred' => 1
+            ],
+            '$limit' => 24
+        ]);
+
+        // 7 days stats
+        $createdWithin7days = $collection->aggregate([
+            [
+                '$group' => [
+                    '_id' => [
+                        'month' => [
+                            '$month' => '$occurred'
+                        ],
+                        'day' => [
+                            '$dayOfMonth' => '$occurred'
+                        ],
+                    ],
+                    'count' => [
+                        '$sum' => 1
+                    ]
+                ]
+            ]
+        ], [
+            '$sort' => [
+                'occurred' => 1
+            ],
+            '$limit' => 7
+        ]);
+
+        // Last 60 minutes
+        $createdWithin60minutes = $collection->aggregate([
+            [
+                '$group' => [
+                    '_id' => [
+                        'month' => [
+                            '$month' => '$occurred'
+                        ],
+                        'day' => [
+                            '$dayOfMonth' => '$occurred'
+                        ],
+                        'hour' => [
+                            '$hour' => '$occurred'
+                        ],
+                        'minute' => [
+                            '$minute' => '$occurred'
+                        ]
+                    ],
+                    'count' => [
+                        '$sum' => 1
+                    ]
+                ]
+            ]
+        ], [
+            '$sort' => [
+                'occurred' => 1
+            ],
+            '$limit' => 60
+        ]);
+
+        // Last 60 minutes
+        $createdWithinLastMonth = $collection->aggregate([
+            [
+                '$group' => [
+                    '_id' => [
+                        'month' => [
+                            '$month' => '$occurred'
+                        ],
+                        'day' => [
+                            '$dayOfMonth' => '$occurred'
+                        ]
+                    ],
+                    'count' => [
+                        '$sum' => 1
+                    ]
+                ]
+            ]
+        ], [
+            '$sort' => [
+                'occurred' => 1
+            ],
+            '$limit' => 31
+        ]);
+
+        return $this->render(':error:stats.html.twig', [
+            'createdWithin24Hours' => $createdWithin24hours->toArray(),
+            'createdWithin7Days' => $createdWithin7days->toArray(),
+            'createdWithin60minutes' => $createdWithin60minutes->toArray(),
+            'createdWithinLastMonth' => $createdWithinLastMonth->toArray()
+        ]);
+    }
+
+    /**
      * @Route("/clear/{app}/{env}", name="error_clear")
      */
     public function clearAction($app, $env, Request $request)
