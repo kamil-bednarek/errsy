c9eedb9fa42067990d62fcd3a186e9b5028ad36b
Kamil Bednarek <kamillo1888@gmail.com>
2016-09-01 10:46:16 +0200

    BSON serialization - Fallback

--- src/AppBundle/Document/Error.php
+++ src/AppBundle/Document/Error.php
@@ -7,12 +7,13 @@
  */
 namespace AppBundle\Document;
 
+use MongoDB\BSON\Persistable;
 use ONGR\ElasticsearchBundle\Annotation as ES;
 
 /**
  * @ES\Document()
  */
-class Error
+class Error implements Persistable
 {
     /**
      * @var string
@@ -118,4 +119,52 @@ class Error
      * @ES\Property(name="method", type="string")
      */
     public $method;
+
+    /**
+     * Serialize BSON to save in MongoDB
+     *
+     * @return array
+     */
+    public function bsonSerialize()
+    {
+        return [
+            '_id' => $this->id,
+            'errorClass' => $this->errorClass,
+            'message' => $this->message,
+            'occurred' => $this->occurred,
+            'backtrace' => $this->backtrace,
+            'parametersPost' => $this->parametersPost,
+            'parametersSession' => $this->parametersSession,
+            'parametersCookie' => $this->parametersCookie,
+            'serverEnv' => $this->serverEnv,
+            'ips' => $this->ips,
+            'env' => $this->env,
+            'app' => $this->app,
+            'user' => $this->user,
+            'method' => $this->method,
+        ];
+    }
+
+    /**
+     * Deserialize BSON to PHP format from MongoDB
+     *
+     * @param array $data
+     */
+    public function bsonUnserialize(array $data)
+    {
+        $this->id = $data['_id'];
+        $this->errorClass = $data['errorClass'];
+        $this->message = $data['message'];
+        $this->occurred = $data['occurred'];
+        $this->backtrace = $data['backtrace'];
+        $this->parametersPost = $data['parametersPost'];
+        $this->parametersSession = $data['parametersSession'];
+        $this->parametersCookie = $data['parametersCookie'];
+        $this->serverEnv = $data['serverEnv'];
+        $this->ips = $data['ips'];
+        $this->env = $data['env'];
+        $this->app = $data['app'];
+        $this->user = $data['user'];
+        $this->method = $data['method'];
+    }
 }
